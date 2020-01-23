27065e43c9455abd64d86c7d394b9cffe30e0a91
Sarah Schwartz <seschwartz8@gmail.com>
2020-01-23 11:14:28 -0800

    complete specs 1 and 2, ignore non-alpha chars and add way with vowel first words

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-
+function toPigLatin(input){
+  var vowels = ["a", "e", "i", "o", "u"];
+  var result = input;
+  var firstChar = input.charAt(0);
+  //If first char is vowel add "way" at end of word
+  if (vowels.includes(firstChar)){
+    result += "way";
+  }
+  return result;
+}
 
 $(document).ready(function(){
 
@@ -7,7 +16,7 @@ $(document).ready(function(){
     event.preventDefault();
 
     var userInput = $("#user-input").val();
-    $("#result").text(userInput);
+    $("#result").text(toPigLatin(userInput));
 
 
   })
